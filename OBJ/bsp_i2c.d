de9f63be30e67ef282b975f4d5eac41e9488ea96
captin2 <114236153+captin2@users.noreply.github.com>
2022-11-04 18:19:05 +0800

    projects

---
+++ OBJ/bsp_i2c.d
@@ -0,0 +1,20 @@
+..\obj\bsp_i2c.o: ..\USER\bsp_i2c.c
+..\obj\bsp_i2c.o: ..\USER\bsp_i2c.h
+..\obj\bsp_i2c.o: ..\SYSTEM\sys\sys.h
+..\obj\bsp_i2c.o: ..\USER\stm32f10x.h
+..\obj\bsp_i2c.o: ..\CORE\core_cm3.h
+..\obj\bsp_i2c.o: D:\mdk531\install index\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\bsp_i2c.o: ..\USER\system_stm32f10x.h
+..\obj\bsp_i2c.o: ..\USER\stm32f10x_conf.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\stm32f10x_dbgmcu.h
+..\obj\bsp_i2c.o: ..\USER\stm32f10x.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\stm32f10x_gpio.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\stm32f10x_rcc.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\stm32f10x_usart.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\misc.h
+..\obj\bsp_i2c.o: ..\STM32F10x_FWLib\inc\stm32f10x_spi.h
+..\obj\bsp_i2c.o: ..\SYSTEM\delay\delay.h
+..\obj\bsp_i2c.o: ..\USER\usart.h
+..\obj\bsp_i2c.o: D:\mdk531\install index\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\bsp_i2c.o: ..\USER\gui.h
+..\obj\bsp_i2c.o: D:\mdk531\install index\ARM\ARMCC\Bin\..\include\string.h
---
+++ OBJ/core_cm3.d
@@ -0,0 +1,2 @@
+..\obj\core_cm3.o: ..\CORE\core_cm3.c
+..\obj\core_cm3.o: D:\mdk531\install index\ARM\ARMCC\Bin\..\include\stdint.h
